d8e9878665d82963ab9c8366c457b90509f69e4c
caoyong <gfxcc@caoyongs-MacBook-Pro.local>
2015-08-01 19:33:11 +0800

    start to write request system

---
+++ iShare/BillListWithFriendViewController.h
@@ -0,0 +1,13 @@
+//
+//  BillListWithFriendViewController.h
+//  iShare
+//
+//  Created by caoyong on 7/30/15.
+//  Copyright (c) 2015 caoyong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BillListWithFriendViewController : UIViewController
+
+@end
