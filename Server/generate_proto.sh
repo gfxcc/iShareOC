ebc6fe6de65aead76d55d499e75e621b67fbf15f
caoyong <gfxcc@caoyongs-MBP.fios-router.home>
2015-08-30 16:55:01 -0400

    delete Server folder

--- Server/generate_proto.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-protoc -I ./ --grpc_out=. --plugin=protoc-gen-grpc=`which grpc_cpp_plugin` ./iShare.proto
-protoc -I ./ --cpp_out=. ./iShare.proto
-protoc --objc_out=. --objcgrpc_out=. ./*.proto
-
-echo Fils Generated Success!
